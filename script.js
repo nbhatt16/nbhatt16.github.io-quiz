12bff59c5a25e993ecbd50a43259725068fdbb4e
Neal Bhatt <nealyb@gmail.com>
2019-11-18 21:41:53 -0500

    timer

--- script.js
+++ script.js
@@ -42,17 +42,17 @@ var questions = [{
 }
 ];
 
-// var sec = 400;
-// var time = setInterval(myTimer, 1000);
+var sec = 75;
+var time = setInterval(myTimer, 1000);
 
-// function myTimer() {
-//     document.getElementById('timer').innerHTML = sec + "sec left";
-//     sec--;
-//     if (sec == -1) {
-//         clearInterval(time);
-//         alert("Time out!! :(");
-//     }
-// };
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+    }
+};
 
 var game = {
     questions:questions,
